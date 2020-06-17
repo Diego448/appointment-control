f489b1884148f164c9b28997d8016e1040ebe68c
Diego448 <diegosg448@gmail.com>
2020-06-17 14:29:27 -0500

    Minor refactoring

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
-@app.route("/")
+
+@app.route('/')
 def homepage():
-    return render_template("homepage.html")
+    return render_template('homepage.html')
